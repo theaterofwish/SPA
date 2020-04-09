0b35d48406fb072a945094a9f828b87d6af86592
Franco Isla Massa <franco.im@outlook.com>
2020-04-09 02:33:27 -0300

    debug

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,6 +1,8 @@
 const resolveRoutes = (route) => {
+  console.log (route);
   if (route.length <= 3) {
     let validRoute = route === '/' ? route : '/:id';
+    console.log(validRoute);
     return validRoute;
   }
   return `/${route}`;
